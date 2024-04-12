30867a575e8ab99aa924fa58ec1d5848bd32b4e8
Sayan Bhattacharya <sayan.bhattacharya91@gmail.com>
2024-04-12 17:14:24 +0530

    updated

--- src/main/java/com/jsp/job_portal/controller/ApplicantController.java
+++ src/main/java/com/jsp/job_portal/controller/ApplicantController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jsp.job_portal.dto.ApplicantDetails;
 import com.jsp.job_portal.service.ApplicantService;
@@ -28,7 +29,7 @@ public class ApplicantController {
 		return "applicant-profile.html";
 	}
 	@PostMapping("/complete-profile")
-	public String completeProfile(ApplicantDetails details,@RequestParam File resume,HttpSession session,ModelMap map) {
+	public String completeProfile(ApplicantDetails details,@RequestParam MultipartFile resume,HttpSession session,ModelMap map) {
 		return applicantService.completeProfile(details,resume,session,map);
 	}
 	
--- src/main/java/com/jsp/job_portal/service/ApplicantService.java
+++ src/main/java/com/jsp/job_portal/service/ApplicantService.java
@@ -1,11 +1,15 @@
 package com.jsp.job_portal.service;
 
-import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.jsp.job_portal.dao.PortalUserDao;
 import com.jsp.job_portal.dto.ApplicantDetails;
 import com.jsp.job_portal.dto.PortalUser;
@@ -18,13 +22,15 @@ public class ApplicantService {
 	@Autowired
 	PortalUserDao userDao;
 
-	public String completeProfile(ApplicantDetails details, File resume, HttpSession session, ModelMap map) {
+	public String completeProfile(ApplicantDetails details, MultipartFile resume, HttpSession session, ModelMap map) {
 		PortalUser portalUser = (PortalUser) session.getAttribute("portalUser");
 		if (portalUser == null) {
 			map.put("msg", "Invalid Session");
 			return "home.html";
 		} else {
-			String resumePath = "cloudPath";// Get from cloudinary
+			
+			
+			String resumePath = uploadTocloudinary(resume);;// Get from cloudinary
 			details.setResumePath(resumePath);
 			portalUser.setApplicantDetails(details);
 			portalUser.setProfileComplete(true);
@@ -32,6 +38,30 @@ public class ApplicantService {
 			map.put("msg", "Profile is Completed");
 			return "applicant-home.html";
 		}
+	}
+	
+	public String uploadTocloudinary(MultipartFile file)
+	{
+		Cloudinary cloudinary=new Cloudinary(ObjectUtils.asMap(
+				
+				"cloud_name", "dt4467ngy",
+				  "api_key", "587385237932688",
+				  "api_secret", "fzF2bUmkIbWAGKh9r52gLYU78Jo"));
+				
+				
+		
+		Map resume =null;
+		try
+		{
+			resume=cloudinary.uploader().upload(file.getBytes(),ObjectUtils.emptyMap());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			
+		}
+		return (String) resume.get("url");
+		
 	}
 
 }
